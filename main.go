23c660a3676c965e927c7dd1532b8f64fd84a693
Suryarpan <52556381+Suryarpan@users.noreply.github.com>
2024-10-11 00:56:16 +0530

    use slog instead of zap

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
-	"log"
-	"time"
-
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+	"fmt"
+	"log/slog"
+	"os"
 )
 
-func main()  {
-
-	loggerConf := zap.NewProductionConfig()
-	loggerConf.EncoderConfig.TimeKey = "timestamp"
-	loggerConf.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.RFC3339)
-
-	logger, err := loggerConf.Build()
-	if err != nil {
-		log.Fatal("Could not acquire a logger")
+func loggerConfig() *slog.HandlerOptions {
+	level, ok := os.LookupEnv("CHAT_API_CONFIG")
+	if !ok {
+		level = "prod"
 	}
-	sugar := logger.Sugar()
-	log.Println()
+
+	var logLevel slog.Level
+	switch level {
+	case "dev":
+		logLevel = slog.LevelDebug
+	case "prod":
+		logLevel = slog.LevelInfo
+	default:
+		panic(fmt.Sprintf("could not understand log level: %s\n", logLevel))
+	}
+	fmt.Printf("Log level set to %s\n", logLevel)
+	return &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: true,
+	}
+}
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, loggerConfig()))
+	slog.SetDefault(logger)
+
+	slog.Info("starting chat api server")
+	slog.Debug("using debug level")
+	slog.Info("using info level")
 }
